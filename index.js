23ab472cce6c8dcf6819351e2417ed7169b5b6f9
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-07-07 21:23:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import express, { request, response } from "express";
+import { PORT,mangoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Item } from "./models/itemModel.js";
+import itemsRoute from "./routes/itemsRoute.js"
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(request,response) => {
+    console.log(request);
+    return response.status(234).send('welcome') ;
+     
+
+});
+
+app.use('/items',itemsRoute)
+
+mongoose
+.connect(mangoDBURL)
+.then( () =>{
+   console.log('connected');
+ 
+  
+})
+
+.catch( (error) =>{
+    console.log(error);
+})
+
+app.listen(PORT, () =>{
+    console.log(`current port : ${PORT}`);
+
+});
